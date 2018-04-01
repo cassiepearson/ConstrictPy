524f53fc6be03f23a4916537e1020ad820e7fb60
cnegrich <cnegrich@gmail.com>
2018-03-31 23:29:07 -0700

    Update to the ConstrictR package
    Optional parameters added to WGCNA

--- ConstrictR/adj_matrix.R
+++ ConstrictR/adj_matrix.R
@@ -3,10 +3,11 @@
 # Description: Custom implementation of algorithm calculating the adjacendy matrix. This will
 #              turn correlation data into network data, allowing various network analysis methods to be applied.
 # Required Parameters: df: dataframe
-# Optional Parameters: thresh: the threshold for the adjacendy matrix calculation, recommended 6 or 12,
+# Optional Parameters: signed: To use the signed or unsigned adjacency in the analysis, signed by default,
+#                      thresh: the threshold for the adjacency matrix calculation, recommended changed to 6 for unsigned,
 #                      verbose: Change to true for printing returned results to console
 
-adj_matrix <- function(df,thresh=6,verbose=FALSE){
+adj_matrix <- function(df,signed=TRUE,thresh=12,verbose=FALSE){
   # Create Pearson's correlation matrix
   corr <- cor(df, use="complete.obs", method="pearson")
 
@@ -16,10 +17,14 @@ adj_matrix <- function(df,thresh=6,verbose=FALSE){
   # Create the signed matrix
   signed <- apply(corr, c(1, 2), function(x) 0.5+0.5*x)
 
-  # Apply thresholding parameter (6 or 12 standard) to create adjacentcy matrix
-  adj <- apply(corr, c(1, 2), function(x) x**thresh)
+  # Apply thresholding parameter (6 or 12 standard) to create adjacency matrix
+  if(signed==TRUE){
+    adj <- apply(signed, c(1, 2), function(x) x**thresh)
+  } else {
+    adj <- apply(unsigned, c(1, 2), function(x) x**thresh)
+  }
 
-  # By convention, diagonal of an adj matrix should be all 0's (would be 1 by above calculations)
+  # By convention, diagonal of an adjacency matrix should be all 0's
   for(i in 1:nrow(adj)){
     for(j in 1:ncol(adj)){
       if(i==j){
--- ConstrictR/wcgna.R
+++ ConstrictR/wcgna.R
@@ -1,27 +1,33 @@
 # WCGNA Function
-# 
+#
 # Description: Custom implementation of weighted correlation network analysis (WCGNA). This function
 #              creates and returns the Topological Overlap Matrix (TOM) from the correlation of data
 #              within the dataframe. This will turn corrleation data into network data, allowing
 #              various network analysis methods to be applied.
 # Required Parameters: df: dataframe
-# Optional Parameters: thresh: the threshold for the adjacendy matrix calculation, recommended 6 or 12,
+# Optional Parameters: signed: To use the signed or unsigned adjacency in the analysis, signed by default,
+#                      thresh: the threshold for the adjacency matrix calculation, recommended changed to 6 for unsigned,
 #                      verbose: Change to true for printing returned results to console
 
-wcgna <- function(df,thresh=6,verbose=FALSE){
+wcgna <- function(df,signed=TRUE,thresh=12,verbose=FALSE){
   # Create Pearson's correlation matrix
   corr <- cor(df, use="complete.obs", method="pearson")
-  
+
   # Create the unsigned matrix
   unsigned <- apply(corr, c(1, 2), function(x) abs(x))
-  
+
   # Create the signed matrix
   signed <- apply(corr, c(1, 2), function(x) 0.5+0.5*x)
-  
-  # Apply thresholding parameter (6 or 12 standard) to create adjacentcy matrix
-  adj <- apply(corr, c(1, 2), function(x) x**thresh)
-  
-  # By convention, diagonal of an adj matrix should be all 0's (would be 1 by above calculations)
+
+  # Apply thresholding parameter (6 or 12 standard) to create adjacency matrix
+  if(signed==TRUE){
+    adj <- apply(signed, c(1, 2), function(x) x**thresh)
+  } else {
+    adj <- apply(unsigned, c(1, 2), function(x) x**thresh)
+  }
+
+
+  # By convention, diagonal of an adjacency matrix should be all 0's
   for(i in 1:nrow(adj)){
     for(j in 1:ncol(adj)){
       if(i==j){
@@ -29,12 +35,12 @@ wcgna <- function(df,thresh=6,verbose=FALSE){
       }
     }
   }
-  
+
   # Calculate needed information for creating the Topological Overlap Matrix (TOM)
   # Create two lists of row and column sums
   k <- rowSums(adj)
   l <- colSums(adj)
-  
+
   # Create a matrix where Aij=ki*lj where u is all nodes in the matrix
   m <- matrix(, nrow = nrow(adj), ncol = ncol(adj))
   for(i in 1:nrow(adj)){
@@ -42,7 +48,7 @@ wcgna <- function(df,thresh=6,verbose=FALSE){
       m[i, j] = k[i]*l[j]
     }
   }
-  
+
   # Create the Topological Overlap Matrix (TOM)
   tom <- matrix(, nrow = nrow(adj), ncol = ncol(adj))
   for(i in 1:nrow(adj)){
@@ -54,15 +60,15 @@ wcgna <- function(df,thresh=6,verbose=FALSE){
       }
     }
   }
-  
+
   # Create a results dataframe
   results <- as.data.frame(tom)
   colnames(results) <- colnames(df)
-  
+
   # Print and return results
   if(verbose == TRUE){
     print(results)
   }
   return(results)
-  
+
 } # End desc_stats function
